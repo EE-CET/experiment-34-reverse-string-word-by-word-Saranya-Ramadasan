c3543cf66c371e3ccf6d6d0f55b09f120df9422d
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-08 20:31:10 +0530

    1st commit

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class ReverseWords {
-    
-        // TODO: Read the sentence line
-        // TODO: Split the sentence into words (using split method)
-        // TODO: Iterate backwards through the array of words
-        // TODO: Build and print the reversed sentence
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        if (!sc.hasNextLine()) return;
+        
+        String s = sc.nextLine();
+        String[] words = s.trim().split("\\s+");
+        
+        StringBuilder reversed = new StringBuilder();
+        
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+        
+        System.out.println(reversed.toString());
+        sc.close();
+    }
 }
